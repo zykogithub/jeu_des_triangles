3ee31d060f69b09d542304fd6e3bc0b5ee1e8283
zykogithub <modaherry@gmail.com>
2024-04-28 23:58:42 +0200

    début de menu, j'attends plus que toi Ines, trop hâte qu'on sy remette ensemble Bisous❤

--- code/main.py
+++ code/main.py
@@ -1,5 +1,12 @@
 import pyxel
-
+class English :
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(55,60,"Tape E to play in english",pyxel.frame_count % 16)
+class French :
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(70,80,"F pour jouer en français.",pyxel.frame_count % 16)
 class App :
     def __init__(self) -> None:
         pyxel.init(200,200,title="Jeu des triangle")
@@ -7,9 +14,14 @@ class App :
     def update(self) :
         if pyxel.btnp(pyxel.KEY_Q):
             quit()
+#TODO : faire en sorte que quand on clique sur E ou F, ça amène vers un autre écran
+        elif pyxel.btnp(pyxel.KEY_E):
+            English()
+        elif pyxel.btnp(pyxel.KEY_F):
+            French()
     def draw(self) :
         pyxel.cls(0)
-        texte : str = "Bienvennu dans le jeu des triagles"
-        pyxel.text(34,35,texte,pyxel.frame_count % 16)
+        pyxel.text(34,35,"Tape E to play in english",pyxel.frame_count % 16)
+        pyxel.text(40,45,"F pour jouer en français.",pyxel.frame_count % 16)
         
 App()
