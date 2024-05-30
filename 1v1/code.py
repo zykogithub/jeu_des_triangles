0466190d6d126e21ae971483d408ae3af2e443bc
zykogithub <modaherry@gmail.com>
2024-05-30 12:42:47 +0200

    début des classes formes

---
+++ 1v1/code.py
@@ -0,0 +1,14 @@
+import pyxel
+
+class Penta :
+    def __init__(self,cote) -> None:
+        self.cote=cote
+class Rect (Penta) :
+    def __init__(self) -> None:
+        super.__init__(4)
+class Tri (Rect):
+    def __init__(self) -> None:
+        super().__init__(3)
+# TODO : faire update et draw pour chaque forme
+# TODO : dans le moule jeu, caster d'une classe à l'autre en cas de toucher
+# TODO : permettre de savoir qui attaque et qui défend, vrai question
